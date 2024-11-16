b67e8e017bf47598b6fcf26c47df224422328962
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-11-17 01:10:14 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Journalize by unimanage',
+  title: 'Journalize by Unimanage',
   description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
